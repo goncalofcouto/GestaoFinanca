4ba98214b05607b5adbd167d7dc779579a62dd27
Goncalo Couto <goncalof.couto04@gmail.com>
2026-03-15 00:42:43 +0000

    CRUD user done

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using GestaoFinanca.Data;
 using GestaoFinanca.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestaoFinanca.Controllers
 {
@@ -19,11 +17,63 @@ namespace GestaoFinanca.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddUser(Users user)
+        public async Task<IActionResult> AddUser([FromBody] Users user)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _appDbContext.DbGestaoFinanca.Add(user);
             await _appDbContext.SaveChangesAsync();
+            return Created("User added successfully!",user);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            var users = await _appDbContext.DbGestaoFinanca.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUser(int id)
+        {
+            var user = await _appDbContext.DbGestaoFinanca.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound("User not found!");
+            }
             return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] Users updatedUser)
+        {
+            var user = await _appDbContext.DbGestaoFinanca.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound("User not found!");
+            }
+
+            _appDbContext.Entry(user).CurrentValues.SetValues(updatedUser);
+
+            await _appDbContext.SaveChangesAsync();
+            return StatusCode(201, user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _appDbContext.DbGestaoFinanca.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound("User not found!");
+            }
+
+            _appDbContext.DbGestaoFinanca.Remove(user);
+            await _appDbContext.SaveChangesAsync();
+            return Ok("User deleted successfully!");
         }
     }
 }
--- Models/Users.cs
+++ Models/Users.cs
@@ -1,13 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GestaoFinanca.Models
 {
     public class Users
     {
+        [Key]
         public int Id { get; set; }
 
+
+        [Required(ErrorMessage = "Name is required!")]
+        [MaxLength(30, ErrorMessage = "Name cannot exceed 30 characters!")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Email is required!")]
+        [EmailAddress(ErrorMessage = "Invalid email format!")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required!")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long!")]
         public string Senha { get; set; }
 
     }
